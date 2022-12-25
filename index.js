6de295800c542a880044d83edb0c7b92403b0c86
SilasAlvesTi <silas.ifce.ti@gmail.com>
2022-12-25 17:44:30 -0300

    Agora texto codificado aparece na area correta

--- index.js
+++ index.js
@@ -1,32 +1,55 @@
-const texto = document.querySelector('.input-texto')
-const criptografar = document.querySelector('.botao-criptografar')
-const descriptografar = document.querySelector('.botao-descriptografar')
+const texto = document.querySelector('.input-texto');
+const criptografar = document.querySelector('.botao-criptografar');
+const descriptografar = document.querySelector('.botao-descriptografar');
+const retanguloSuperior = document.querySelector('.retangulo-superior');
+const outputTexto = document.querySelector('.output-texto');
 
+function tornaOutputVisivel(textoCriptogradado) {
+    const imagem = retanguloSuperior;
+    const textArea = outputTexto;
+
+    textArea.value = textoCriptogradado;
+    imagem.style.visibility = 'hidden';
+    textArea.style.visibility = 'visible';
+}
+
+function tornaOutputOculto() {
+    const imagem = retanguloSuperior;
+    const textArea = outputTexto;
+
+    imagem.style.visibility = 'visible';
+    textArea.style.visibility = 'hidden';
+}
+
+function apenasLetrasMinusculas(texto) {
+    const alfabeto = /^[a-z]+$/;
+
+    return alfabeto.test(texto);
+}
 
 criptografar.addEventListener('click', function() {
-    const alfabeto = /^[a-z]+$/;
-    const textoSemEspacos = texto.value.replaceAll(' ', '')
+    const textoSemEspacos = texto.value.replaceAll(' ', '');
  
-    if (!alfabeto.test(textoSemEspacos)) {
-        alert('O texto deve conter apenas letras minúsuclas')
+    if (!apenasLetrasMinusculas(textoSemEspacos)) {
+        alert('O texto deve conter apenas letras minúsuclas');
         return
     }
 
-    texto.value = texto.value.
+    const textoCriptogradado = texto.value.
         replaceAll('e', 'enter').
         replaceAll('i', 'imes').
         replaceAll('a', 'ai').
         replaceAll('o', 'ober').
-        replaceAll('u', 'ufat')
-    
+        replaceAll('u', 'ufat');
+
+    tornaOutputVisivel(textoCriptogradado);
 })
 
 descriptografar.addEventListener('click', function() {
-    const alfabeto = /^[a-z]+$/;
-    const textoSemEspacos = texto.value.replaceAll(' ', '')
+    const textoSemEspacos = texto.value.replaceAll(' ', '');
  
-    if (!alfabeto.test(textoSemEspacos)) {
-        alert('O texto deve conter apenas letras minúsuclas')
+    if (!apenasLetrasMinusculas(textoSemEspacos)) {
+        alert('O texto deve conter apenas letras minúsuclas');
         return
     }
 
@@ -35,6 +58,7 @@ descriptografar.addEventListener('click', function() {
         replaceAll('imes', 'i').
         replaceAll('ai', 'a').
         replaceAll('ober', 'o').
-        replaceAll('ufat', 'u')
-    
+        replaceAll('ufat', 'u');
+
+    tornaOutputOculto();
 })
